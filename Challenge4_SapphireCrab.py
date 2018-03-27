7bcae84b9f66490c7513932762fa739f5ca4fce6
James Shaver <parallelshaver@gmail.com>
2018-03-27 10:53:08 -0500

    Challenge 4 Update

--- Challenge4_SapphireCrab.py
+++ Challenge4_SapphireCrab.py
@@ -1,11 +1,40 @@
 import matplotlib.pyplot as mplot
 import numpy as np
+import glob
 
-x, y, v = 
+try:
+	userInput = float(input('How many Neighbors would you like to sample?: '))
+except ValueError:
+	float(input('Error! -- PLEASE ENTER ONLY NUMBERS!: '))
+
+migrationData = glob.glob('C:/Users/**/SapphireCrab/data.csv', recursive=True)
+migrationData = open(migrationData, 'r')
+migrationData = migrationData.read()
+migrationData = migrationData.split('\n')
+
+mapData = glob.glob('C:/Users/**/SapphireCrab/us_outline.csv', recursive=True)
+mapData = open(mapData, 'r')
+mapData = mapData.read()
+mapData = mapData.split('\n')
+
+data = []
+usMap = []
+
+for info in migrationData:
+	info = info.split(',')
+	data.append(info)
+
+for info in mapData:
+	info = info.split(',')
+	usMap.append(info)
+
+print(data)
+
+# x, y, v =
 
 # x = horizontal grid position
 # y = vertical grid position
 # v = population change reconstructed
 
-mplot.scatter(x, y, c=v, cmap='viridis')
-mplot.show()
+# mplot.scatter(x, y, c=v, cmap='viridis')
+# mplot.show()
--- algorithmSpeed.py
+++ algorithmSpeed.py
@@ -1,14 +1,23 @@
 import matplotlib.mlab as mlab
+import numpy as np
 from time import time
+import math
+
+a = [3.5, 5, 0]
+b = [6, 1.3, 10]
 
 # gets start time
-start = time
+start = time()
 
 # put code to be timed here
-
+# dist = mlab.dist(a[0], b[0])
+# dist = np.sqrt(np.sum((a[0] - b[0]) ** 2))
+# dist = np.sqrt(np.power(a[0] - b[0], 2) + np.power(a[1] - b[1], 2))
+# dist = np.linalg.norm(a - b)
+# dist = math.sqrt(math.pow(a[0] - b[0], 2) + math.pow(a[1] - b[1], 2))
 
 # gets end time
-end = time
+end = time()
 
 # gets total time in seconds
 total_time = end - start
