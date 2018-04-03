f6eb2c19ad7995a74d7d93603986f032f0cdd5a0
James Shaver <parallelshaver@gmail.com>
2018-04-03 14:30:33 -0500

    Challenge4 Complete

--- Challenge4_SapphireCrab.py
+++ Challenge4_SapphireCrab.py
@@ -1,6 +1,26 @@
+'''
+	Name: James Jacob Shaver
+	ID: SapphireCrab
+	Email: jjshaver@go.olemiss.edu
+	Program Source File Name: SapphireCrab.py
+	Last Edit Date: 2 April 2018
+	Course Information: CSci 343
+	Program Description:
+	Sources Consulted:
+	Honor Code Statement: In keeping with the honor code policies of the
+	University of Mississippi, the School of Engineering, and the Department of
+	Computer and Information Science, I affirm that I have neither given nor
+	received assistance on this programming assignment. This assignment
+	represents my individual, original effort.
+		...My Signature is on File.
+	'Lasciate ogne speranza, voi ch'intrate'
+'''
+import math
+import matplotlib.mlab as mlab
 import matplotlib.pyplot as mplot
 import numpy as np
 import glob
+import csv
 
 try:
 	userInput = float(input('How many Neighbors would you like to sample?: '))
@@ -8,33 +28,51 @@ except ValueError:
 	float(input('Error! -- PLEASE ENTER ONLY NUMBERS!: '))
 
 migrationData = glob.glob('C:/Users/**/SapphireCrab/data.csv', recursive=True)
-migrationData = open(migrationData, 'r')
-migrationData = migrationData.read()
-migrationData = migrationData.split('\n')
+
+for moveData in migrationData:
+	mapData = open(moveData, 'r')
+	read = csv.reader(mapData)
+	data = list(read)
 
 mapData = glob.glob('C:/Users/**/SapphireCrab/us_outline.csv', recursive=True)
-mapData = open(mapData, 'r')
-mapData = mapData.read()
-mapData = mapData.split('\n')
 
-data = []
-usMap = []
+for cartogropher in mapData:
+	cData = open(cartogropher, 'r')
+	read = csv.reader(cData)
+	mData = list(read)
 
-for info in migrationData:
-	info = info.split(',')
-	data.append(info)
+x = []
+y = []
+v = []
+avg = 0
 
-for info in mapData:
-	info = info.split(',')
-	usMap.append(info)
+for i in range(0, 194):
+	for k in range(0, 120):
+		distance = []
+		for j in data:
+			a = [i, k]
+			b = [int(j[0]), int(j[1])]
+			dist = math.sqrt(math.pow(a[0] - b[0], 2) + math.pow(a[1] - b[1], 2))
+			distance.append([dist, np.float64(j[2])])
+		distance = sorted(distance, key=lambda val: val[0])
+		distance = distance[0: int(userInput)]
+		for l in distance:
+			avg += l[1]
+		avg = avg / userInput
+		v.append(avg)
+		x.append(i)
+		y.append(k)
 
-print(data)
+xLine = []
+yLine = []
 
-# x, y, v =
+for row in mData:
+	xLine.append(float(row[0]))
 
-# x = horizontal grid position
-# y = vertical grid position
-# v = population change reconstructed
+for col in mData:
+	yLine.append(float(col[1]))
 
-# mplot.scatter(x, y, c=v, cmap='viridis')
-# mplot.show()
+mplot.title('Domestic Migration in the United States of America')
+mplot.plot(xLine, yLine, color='black', linestyle='-')
+mplot.scatter(x, y, c=v, cmap='viridis')
+mplot.show()
