3e8757bccc98dd716f46bdc90e82fefe99ed7225
James Shaver <parallelshaver@gmail.com>
2018-05-08 14:00:33 -0500

    Close to done with Final

---
+++ AStarSearch.py
@@ -0,0 +1,85 @@
+import heapq
+
+
+class Cell(object):
+	def __init__(self, x, y, reachable):
+		self.reachable = reachable
+		self.x = x
+		self.y = y
+		self.parent = None
+		self.g = 0
+		self.h = 0
+		self.f = 0
+
+
+class AStar(object):
+	"""docstring for AStar"""
+
+	def __init__(self):
+		self.opened = []
+		heapq.heapify(self.opened)
+		self.closed = set()
+		self.cells = []
+		self.gridHeight = 5
+		self.gridWidth = 5
+
+	def initGrid(self, width, height, walls, start, end):
+		self.grid_height = height
+		self.grid_width = width
+		for x in range(self.grid_width):
+			for y in range(self.grid_height):
+				if (x, y) in walls:
+					reachable = False
+				else:
+					reachable = True
+				self.cells.append(Cell(x, y, reachable))
+		self.start = self.get_cell(*start)
+		self.end = self.get_cell(*end)
+
+	def getHeuristic(self, cell):
+		return 10 * (abs(cell.x - self.end.x) + abs(cell.y - self.end.y))
+
+	def getCell(self, x, y):
+		return self.cells[x * self.gridHeight + y]
+
+	def getAdjacentCells(self, cell):
+		cells = []
+		if cells.x < self.gridWidth - 1:
+			cells.append(self.getCell(cell.x + 1, cell.y))
+		if cell.y > 0:
+			cells.append(self.getCell(cell.x, cell.y))
+		if cell.x > 0:
+			cells.append(self.getCell(cell.x - 1, cell.y))
+		if cell.y < self.gridHeight - 1:
+			cells.append(self.getCell(cell.x, cell.y + 1))
+		return cells
+
+	def displayPath(self):
+		cell = self.end
+		while cell.parent is not self.start:
+			cell = cell.parent
+			print('path: cell: %d, %d' % (cell.x, cell.y))
+
+	def updateCell(self, adj, cell):
+		adj.g = cell.g + 10
+		adj.h = self.getHeuristic(adj)
+		adj.parent = cell
+		adj.f = adj.h + adj.g
+
+	def process(self):
+		heapq.heappush(self.opened, (self.start.f, self.start))
+		while len(self.opened):
+			f, cell = heapq.heappop(self.opened)
+			self.closed.add(cell)
+			if cell is self.end:
+				self.displayPath()
+				break
+			adjCells = self.getAdjacentCells(cell)
+			for adjCell in adjCells:
+				if adjCell.reachable and adjCell not in self.closed:
+					if(adjCell.f, adjCell) in self.opened:
+						if adjCell.g > cell.g + 10:
+							self.updateCell(adjCell, cell)
+				else:
+					self.updateCell(adjCell, cell)
+					heapq.heappush(self.opened, (adjCell.f, adjCell))
--- Final_SapphireCrab.py
+++ Final_SapphireCrab.py
@@ -22,23 +22,51 @@
 import neuro
 import random
 import glob
+# import aStarSearch as aStar
 
 myNetwork = glob.glob('C:/Users/**/myNetwork.csv', recursive=True)
+
 count = 0
 userInput = float(input('\nHow many times do you' +
-			'want to run the a* search algorithm?: '))
+				' want to run the Search algorithm?: '))
+sInfo = []
+
+
+def search(x, y):
+	if grid[x][y] == 2:
+		print('Solved... This is a Maze!')
+		sInfo.append(1)
+		return True
+	elif grid[x][y] == 1:
+		print('Wall! at %d, %d' % (x, y))
+		return False
+	elif grid[x][y] == 3:
+		print('Visiting %d, %d' % (x, y))
+		return False
+	print('Not a Maze!')
+	grid[x][y] = 3
+	if ((x < len(grid) - 1 and search(x + 1, y))
+		or (y > 0 and search(x, y - 1))
+		or (x > 0 and search(x - 1, y))
+		or (y < len(grid) - 1 and search(x, y + 1))):
+		return True
+	return False
+
 
 while count != userInput:
 	seq = []
 	for i in range(5):
-		seq.append(random.randint(0, 1))
+		seq.append((random.randint(0, 1)))
 		for k in range(4):
-			seq.append(random.randint(0, 1))
-
-	print(seq)
+			seq.append((random.randint(0, 1)))
 
 	if seq[0] == 0 and seq[-1] == 0:
+		seq[-1] = 2
 		grid = [seq[:5], seq[5:10], seq[10:15], seq[15:20], seq[20:]]
 		count += 1
+		print(grid)
 	else:
 		print('A maze this is not... Skipping')
+	search(0, 0)
+
+print(sInfo)
