a3b1e9f3758edfdbf009158ef653338ba5611ce8
James Shaver <parallelshaver@gmail.com>
2018-05-08 22:59:04 -0500

    Something Soemthing Smeothingwleoisnmfe

--- Last_SapphireCrab.py
+++ Last_SapphireCrab.py
@@ -19,59 +19,19 @@
 			'Was wir für uns selbst tun, stirbt mit uns.
 	Was wir für andere und die Welt tun, bleibt und ist unsterblich.'
 '''
-import neuro
 import random
-import os
+import neuro
 
-# inputs = open('myNetwork.csv', 'r')
-# inputs = inputs.read()
-# inputs = inputs.split('\n')
-# if inputs[-1] == '':
-# 	inputs.pop()
-
-# newT = []
-# targets = open('myTest.csv', 'r')
-# targets = targets.read()
-# targets = targets.split('\n')
-# if targets[-1] == '':
-# 	targets.pop()
-# for i in targets:
-# 	newT.append(i.split(','))
-# 	print(float(newT))
-
-
-
+size = 5
+count = 0
+grid = []
+target = []
+inputs = []
 reps = 1000
 network = []
-netowrk = neuro.setup_network(inputs)
-neuro.train(network, inputs, targets, reps)
-neuro.writeNetworkToFile('myNetwork.net', network)
-
-count = 0
 myGrid = open('myNetwork.csv', 'w+')
 
-
-def search(x, y):
-		if grid[x][y] == 2:
-			print('Solved... This is a Maze!')
-			return True
-		elif grid[x][y] == 1:
-			print('Wall! at %d, %d' % (x, y))
-			return False
-		elif grid[x][y] == 3:
-			print('Visiting %d, %d' % (x, y))
-			return False
-		print('Visiting %d, %d' % (x, y))
-		grid[x][y] = 3
-		if ((x < len(grid) - 1 and search(x + 1, y))
-			or (y > 0 and search(x, y - 1))
-			or (x > 0 and search(x - 1, y))
-			or (y < len(grid) - 1 and search(x, y + 1))):
-			return True
-		return False
-
-
-while count != 200:
+while count != 5:
 	seq = []
 	newSeq = []
 	for i in range(5):
@@ -81,10 +41,15 @@ while count != 200:
 	if seq[0] == 0 and seq[-1] == 0:
 		for i in seq:
 			newSeq.append(float(i))
-		grid = [newSeq[0:5], newSeq[5:10], newSeq[10:15], newSeq[15:20], newSeq[20:]]
+		grid = newSeq[0:5], newSeq[5:10], newSeq[10:15], newSeq[15:20], newSeq[20:]
 		count += 1
-		
-		myGrid.write(str(grid) + '\n')
+		inputs.append([newSeq[0:5], newSeq[5:10], newSeq[10:15], newSeq[15:20], newSeq[20:]])
+		# myGrid.write(str(grid) + '\n')
+		# network = neuro.setup_network(grid)
+		# neuro.train(network, grid, target, reps)
+		# neuro.writeNetworkToFile('myNetwork.net', network)
+
 	else:
 		print('A maze this is not... Skipping')
-	print(grid)
+for i in inputs:
+	print(i)
