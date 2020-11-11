18b177ecef90354245edeb821444691af75cf917
James Shaver <parallelshaver@gmail.com>
2020-11-11 09:24:48 -0600

    Modified all files in some shape and form. Scrapy is not working 100%, Neural network is coming along, readMe is in processing.

--- club_durian/Test.py
+++ club_durian/Test.py
@@ -1,7 +1,26 @@
+'''
+	Name: James Jacob Shaver
+	Email: jjshaver@go.olemiss.edu
+	Program Source File Name: README.md
+	Last Edit Date: 11 November 2020
+	Course Information: CSci 487
+	Program Description: This is a readme file on how to use my NeuralNetwork
+	Sources Consulted:
+	Honor Code Statement: In keeping with the honor code policies of the
+	University of Mississippi, the School of Engineering, and the Department of
+	Computer and Information Science, I affirm that I have neither given nor
+	received assistance on this programming assignment. This assignment
+	represents my individual, original effort.
+		...My Signature is on File.
+
+			'Was wir für uns selbst tun, stirbt mit uns.
+	Was wir für andere und die Welt tun, bleibt und ist unsterblich.'
+'''
+
 import requests
 count = 5
 
-url = 'https://thingiverse.com/thing:' + str(count) + '/zip'
+url = 'https://thingiverse.com/thing:5503'
 
 #use the 'headers' parameter to set the HTTP headers:
 x = requests.head(url)
--- club_durian/thingiVerseScrape.py
+++ club_durian/thingiVerseScrape.py
@@ -1,25 +1,34 @@
+'''
+	Name: James Jacob Shaver
+	Email: jjshaver@go.olemiss.edu
+	Program Source File Name: README.md
+	Last Edit Date: 11 November 2020
+	Course Information: CSci 487
+	Program Description: This is a readme file on how to use my NeuralNetwork
+	Sources Consulted:
+	Honor Code Statement: In keeping with the honor code policies of the
+	University of Mississippi, the School of Engineering, and the Department of
+	Computer and Information Science, I affirm that I have neither given nor
+	received assistance on this programming assignment. This assignment
+	represents my individual, original effort.
+		...My Signature is on File.
+
+			'Was wir für uns selbst tun, stirbt mit uns.
+	Was wir für andere und die Welt tun, bleibt und ist unsterblich.'
+'''
+
 import os, requests
 from tqdm import tqdm
-count = 0
+count = 5503
 chunk = 1024
 url = 'https://thingiverse.com/thing:' + str(count) + '/zip'
 req = requests.get(url, stream=True)
-size = int(req.head['Content-length'])
-with open('F:/Final Project Jank/File' + str(count) + '.zip', 'wb') as zip:
-		for data in tqdm(iterable=req.iter_content(chuck_size=chunk), total=size/chunk, unit='KB'):
+size = int(req.headers.get('Content-Length', 0))
+filename = url.split('- Thingiverse')[-1]
+print(filename)
+progress = tqdm(req.iter_content(chunk), f'Downloading {filename}', 
+			total=size, unit='B', unit_scale=True, unit_divisor=1024)
+with open('filename', 'wb') as zip:
+		for data in progress:
 			zip.write(req.content)
-print('Download: ' + str(i) + ' Complete!')
-# for i in range(1, 200):
-# 	with open('F:/Final Project Jank/File' + str(count) + '.zip', 'wb') as zip:
-# 		for data in tqdm(iterable=req.iter_content(chuck_size=chunk), total=size/chunk, unit='KB'):
-# 			zip.write(req.content)
-# 	print('Download: ' + str(i) + ' Complete!')
-# 	count += 1
-	
-# for i in range (1, 10):
-# 	r = requests.get(url, stream=True)
-# 	print("thing:" + str(i) + " status: " + str(r.status_code))
-# 	if r.status_code == 200:
-# 		f = open("thing_" + str(i) + ".zip", "wb")
-# 		f.write(r.content)
-# 		f.close()
+			progress.update(len(data))
